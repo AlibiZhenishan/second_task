9d7a7dde9225f6358206ada7c6ebfb0caebf6a4f
Alibi Zhenishan <alibizhenishan@gmail.com>
2023-07-06 21:40:23 +0600

    changed hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
 print('Hello world')
+def name ():
+    supname = 0
+    while supname == 0:
+        name = input('What is your name? ')
+        print('Is your name ', name, 'yes/no')
+        a = input()
+        if a =='yes':
+            supname = 1
+    print('Hello', name)
+name()
